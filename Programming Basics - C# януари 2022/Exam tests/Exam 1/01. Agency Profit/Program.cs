5176560710564d1c062c4406e797867385fb23f7
Alex <er_04@abv.bg>
2022-05-26 23:50:47 +0300

    Added Exam - №1......

---
+++ Programming Basics - C# януари 2022/Exam tests/Exam 1/01. Agency Profit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exam_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string avionCompany = Console.ReadLine();
+            int elderlyTickets = int.Parse(Console.ReadLine());
+            int youthTickets = int.Parse(Console.ReadLine());
+            double netWorthPrice = double.Parse(Console.ReadLine());
+            double serviceFee = double.Parse(Console.ReadLine());
+
+            double netWorthPriceYouth = netWorthPrice - (0.7 * netWorthPrice);
+            double elderlyTicketsPrice = netWorthPrice + serviceFee;
+            double youthTicketsPrice = netWorthPriceYouth + serviceFee;
+            double totalPrice = (elderlyTickets * elderlyTicketsPrice) + (youthTickets * youthTicketsPrice);
+            double profit = 0.2 * totalPrice;
+
+            Console.WriteLine($"The profit of your agency from {avionCompany} tickets is {profit:f2} lv.");
+
+
+
+        }
+    }
+}
---
+++ Programming Basics - C# януари 2022/Exam tests/Exam 1/05. Best Player/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _05._Best_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = true;
+
+            string name = Console.ReadLine();
+            int goals = int.Parse(Console.ReadLine());
+
+            string bestPlayer = "";
+            int mostGoals = 0;
+
+            while (check)
+            {
+                if (goals > mostGoals)
+                {
+                    bestPlayer = name;
+                    mostGoals = goals;
+                }
+                if (goals >= 10) break;
+                name = Console.ReadLine();
+                if (name == "END") break;
+                else goals = int.Parse(Console.ReadLine());
+            }
+            
+
+            Console.WriteLine($"{bestPlayer} is the best player!");
+            if (mostGoals >= 3) Console.WriteLine($"He has scored {mostGoals} goals and made a hat-trick !!!");
+            else Console.WriteLine($"He has scored {mostGoals} goals");
+
+            
+
+                
+               
+
+
+
+
+        }
+    }
+}
