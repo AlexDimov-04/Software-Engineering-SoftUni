da0365c60f4976e612c4900d5735ac25917fcc4e
Alexander Dimov <106152399+AlexDimov-04@users.noreply.github.com>
2022-06-16 17:27:53 +0300

    Add files via upload

---
+++ Programming Fundametals - Python май 2022/Homeworks - Labs and Exercises/Lists Advanced - Lab/03. To-do List.py
@@ -0,0 +1,14 @@
+tasks = []
+
+while True:
+    command = input().split('-')
+
+    if command[0] == 'End':
+        break
+
+    priority = int(command[0])
+    task = command[1]
+    tasks.append((priority, task))
+
+sorted_tasks = [task_data[1] for task_data in sorted(tasks)]
+print(sorted_tasks)
