28034d8382d62996c42356e95588ba270846480d
Alexander Dimov <106152399+AlexDimov-04@users.noreply.github.com>
2022-06-16 17:38:10 +0300

    Add files via upload

---
+++ Programming Fundametals - Python май 2022/Homeworks - Labs and Exercises/Lists Advanced - Lab/04. Palindrome Strings.py
@@ -0,0 +1,13 @@
+def palindrome_filter(word: str):
+    if word == word[::-1]:
+        return word
+
+
+words = input().split(' ')
+palindrome = input()
+
+palindrome_list = [word for word in words if palindrome_filter(word)]
+print(palindrome_list)
+print(f"Found palindrome {palindrome_list.count(palindrome)} times")
+
+
