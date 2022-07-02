5af0118e674d504037c60c8b1f1ff9d739856bd5
Alexander Dimov <106152399+AlexDimov-04@users.noreply.github.com>
2022-07-02 20:30:12 +0300

    Add files via upload

---
+++ Programming Fundametals - Python май 2022/Homeworks - Labs and Exercises/Objects and Classes - Exericse/08. Vehicle.py
@@ -0,0 +1,27 @@
+class Vehicle:
+    def __init__(self, type, model, price, owner=None):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = owner
+
+    def buy(self, money: int, owner: str):
+        if money >= self.price and self.owner is None:
+            self.owner = owner
+            change = money - self.price
+            return f"Successfully bought a {self.type}. Change: {change:.2f}"
+        elif money < self.price:
+            return "Sorry, not enough money"
+        else:
+            return "Car already sold"
+
+    def sell(self):
+        if self.owner is not None:
+            self.owner = None
+        else:
+            return "Vehicle has no owner"
+
+    def __repr__(self):
+        if self.owner:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        return f"{self.model} {self.type} is on sale: {self.price}"
