bdf223ea6ac1f7feb01d0d04fe1da5263dde16f6
Alexander Dimov <alex.lemuel@gmail.com>
2023-03-01 22:16:03 +0200

    add

--- JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/Functions and Statements - Lab/01. Format Grade.js
+++ JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/Functions and Statements - Lab/01. Format Grade.js
@@ -8,7 +8,7 @@ function grade(grade) {
     else if (grade >= 3.50 && grade < 4.50) {
         console.log(`Good (${grade.toFixed(2)})`);
     }
-    else if (grade >= 3.50 && grade < 5.50) {
+    else if (grade >= 4.50 && grade < 5.50) {
         console.log(`Very good (${grade.toFixed(2)})`);
     } else {
         console.log(`Excellent (${grade.toFixed(2)})`)
