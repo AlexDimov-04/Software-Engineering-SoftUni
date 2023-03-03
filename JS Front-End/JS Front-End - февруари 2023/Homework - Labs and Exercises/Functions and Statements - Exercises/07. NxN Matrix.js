8e709268f5e22d08bbe046b4b6903a9f52021061
Alexander Dimov <alex.lemuel@gmail.com>
2023-03-03 17:56:22 +0200

    added exercises

--- JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/Functions and Statements - Exercises/07. NxN Matrix.js
+++ JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/Functions and Statements - Exercises/07. NxN Matrix.js
@@ -1,9 +1,17 @@
 function matrix(n) {
-    for (let r = 0; r < n; r++) {
-        let row = '';
-        for (let c = 0; c < n; c++) {
-            row += n + ' ';
-        } 
-        console.log(row);
-    }
+    let matrix = new Array(n).fill(new Array(n).fill(n));
+
+    matrix.forEach(arr => {
+        console.log(arr.join(' '));
+    });
 }
+
+// function matrix(n) {
+//     for (let r = 0; r < n; r++) {
+//         let row = '';
+//         for (let c = 0; c < n; c++) {
+//             row += n + ' ';
+//         } 
+//         console.log(row);
+//     }
+// }
