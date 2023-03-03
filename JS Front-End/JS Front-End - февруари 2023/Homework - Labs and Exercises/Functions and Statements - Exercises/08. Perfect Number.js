2a463d444e249f13133a61a6aaeecb6eb873dac6
Alexander Dimov <alex.lemuel@gmail.com>
2023-03-03 18:16:33 +0200

    added exercises

--- JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/Functions and Statements - Exercises/08. Perfect Number.js
+++ JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/Functions and Statements - Exercises/08. Perfect Number.js
@@ -1,16 +1,11 @@
 function perfectNumber(n) {
-    let halfNum = n / 2;
     let sum = 0;
 
-    for (let index = 1; index <= halfNum; index++) {
+    for (let index = 1; index <= n / 2; index++) {
         if (n % index === 0) {
             sum += index;
         } 
     }
 
-    if (sum === n) {
-        console.log("We have a perfect number!");
-    } else {
-        console.log("It's not so perfect.");
-    }
+    sum === n ? console.log("We have a perfect number!") : console.log("It's not so perfect.");
 }
