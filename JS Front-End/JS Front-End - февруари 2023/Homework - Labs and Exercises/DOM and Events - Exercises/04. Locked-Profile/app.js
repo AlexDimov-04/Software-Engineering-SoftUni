d77f347daee21eef103233e26979c5ab73dd5f4e
Alexander Dimov <alex.lemuel@gmail.com>
2023-03-19 21:32:16 +0200

    added exercises

--- JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/DOM and Events - Exercises/04. Locked-Profile/app.js
+++ JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/DOM and Events - Exercises/04. Locked-Profile/app.js
@@ -1,5 +1,26 @@
 function lockedProfile() {
     const btns = Array.from(document.getElementsByTagName('button'));
+    btns
+        .forEach((btn) => {
+            btn.addEventListener('click', toggleInformation);
+        });
 
+    function toggleInformation(e) {
+        debugger;
+        const btn = e.currentTarget;
+        const currentProfile = btn.parentNode;
+        const children = Array.from(currentProfile.children);
+        const unlockRadioInput = children[4];
+        const additionalInformationDiv = children[9];
 
+        if (unlockRadioInput.checked) {
+            if (btn.textContent === 'Show more') {
+                additionalInformationDiv.style.display = 'block';
+                btn.textContent = 'Hide it';
+            } else {
+                additionalInformationDiv.style.display = 'none';
+                btn.textContent = 'Show more';
+            }
+        }
+    }
 }
--- JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/DOM and Events - Exercises/07. Format the Text/solution.js
+++ JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/DOM and Events - Exercises/07. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
-}
+  const textarea = document.getElementById('input');
+  const output = document.getElementById('output');
+  let sentences = textarea.value.split('.');
+  sentences.pop();
+
+  while (sentences.length > 0) {
+    let paragraphSentences = sentences.splice(0, 3)
+      .map((p) => p.trimStart());
+
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = paragraphSentences.join('.') + '.';
+
+    output.appendChild(newParagraph);
+  }
+} 
