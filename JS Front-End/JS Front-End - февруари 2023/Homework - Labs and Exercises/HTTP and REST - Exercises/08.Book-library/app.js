b88c27b428e4864cfb5d5888f9ee31139c9c8748
Alexander Dimov <alex.lemuel@gmail.com>
2023-03-28 00:56:32 +0300

    added solution

--- JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/HTTP and REST - Exercises/08.Book-library/app.js
+++ JS Front-End/JS Front-End - февруари 2023/Homework - Labs and Exercises/HTTP and REST - Exercises/08.Book-library/app.js
@@ -1,5 +1,91 @@
 function attachEvents() {
-  // TODO:
+  const loadBooks = document.getElementById('loadBooks');
+  const booksContainer = document.querySelector('table > tbody');
+  const [titleInput, authorInput] = Array.from(document.querySelectorAll('#form > input'));
+  const submitBtn = document.querySelector('#form > button');
+  const formHeader = document.querySelector('#form > h3');
+  let editBookId = null;
+  const BASE_URL = 'http://localhost:3030/jsonstore/collections/books/';
+
+  loadBooks.addEventListener('click', loadAllBooksHandler);
+  submitBtn.addEventListener('click', submitFormHandler);
+
+  async function loadAllBooksHandler() {
+    booksContainer.innerHTML = '';
+    const booksRes = await fetch(BASE_URL);
+    const booksData = await booksRes.json();
+    allBooks = booksData;
+
+    for (const bookId in booksData) {
+      const { author, title } = booksData[bookId];
+      const tableRow = document.createElement('tr');
+      const titleColumn = document.createElement('td');
+      const authorColumn = document.createElement('td');
+      const buttonsColumn = document.createElement('td');
+      const editBtn = document.createElement('button');
+      const deleteBtn = document.createElement('button');
+
+      titleColumn.textContent = title;
+      authorColumn.textContent = author;
+      editBtn.textContent = 'Edit';
+      deleteBtn.textContent = 'Delete';
+      deleteBtn.id = bookId;
+
+      editBtn.addEventListener('click', () => {
+        editBookId = bookId;
+        formHeader.textContent = 'Edit FORM';
+        submitBtn.textContent = 'Save';
+        titleInput.value = title;
+        authorInput.value = author;
+      });
+
+      deleteBtn.addEventListener('click', deleteBookHandler);
+
+      tableRow.appendChild(titleColumn);
+      tableRow.appendChild(authorColumn);
+      buttonsColumn.appendChild(editBtn);
+      buttonsColumn.appendChild(deleteBtn);
+      tableRow.appendChild(buttonsColumn)
+
+      booksContainer.appendChild(tableRow);
+    }
+  }
+
+  async function submitFormHandler() {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const httpHeaders = {
+      method: 'POST',
+      body: JSON.stringify({ title, author })
+    };
+
+    let url = BASE_URL;
+
+    if (formHeader.textContent === 'Edit FORM') {
+      httpHeaders.method = 'PUT';
+      url += editBookId;
+    }
+
+    const resData = await fetch(url, httpHeaders);
+    loadAllBooksHandler();
+    if (formHeader.textContent === 'Edit FORM') {
+      formHeader.textContent = 'FORM';
+      submitBtn.textContent = 'Submit';
+    }
+
+    titleInput.value = '';
+    authorInput.value = '';
+  }
+
+  async function deleteBookHandler() {
+    const id = this.id;
+    const httpHeaders = {
+      method: "DELETE"
+    };
+
+    await fetch(BASE_URL + id, httpHeaders);
+    loadAllBooksHandler();
+  }
 }
 
 attachEvents();
