c5efc20de00d0d9ac5eaef85f3da947ddf93b439
Alexander Dimov <alex.lemuel@gmail.com>
2023-05-22 01:31:57 +0300

    added feature

--- Python Web/Python Web Basics/departments_project/departments/urls.py
+++ Python Web/Python Web Basics/departments_project/departments/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import index, details, details_template
+from .views import index, details, details_template, details_error
 
 urlpatterns = [
-    path("", index),
-    path("int/<int:department_id>/", details),
-    path("template/<int:department_id>/", details_template),
-    path("<department_id>/", details)
+    path("", index, name="index page"),
+    path("int/<int:department_id>/", details, name="departments int"),
+    path(
+        "template/<int:department_id>/", details_template, name="departments template"
+    ),
+    path("error/<int:department_id>/", details_error, name="departments error"),
+    path("<department_id>/", details, name="department only id"),
 ]
 
 
--- Python Web/Python Web Basics/departments_project/departments/views.py
+++ Python Web/Python Web Basics/departments_project/departments/views.py
@@ -1,35 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.http.response import Http404
+
 
 def index(request):
     print(request)
-    return HttpResponse('index')
+    return HttpResponse("index")
 
 
 def details(request, department_id):
-    departments_map = {
-        '1': "Developers",
-        '2': "QA"
-    }
+    departments_map = {"1": "Developers", "2": "QA"}
 
     payload = f"Department: {departments_map.get(str(department_id), 'Unknown')}"
     # return HttpResponse(payload)
     # return redirect('/departments/template/', department_id=department_id)
-    return 
+    # return redirect('https:softuni.bg')
+    return redirect("departments template", department_id=department_id)
+
 
 def details_template(request, department_id):
-    departments_map = {
-        '1': "Developers",
-        '2': "QA"
-    }
+    departments_map = {"1": "Developers", "2": "QA"}
 
     payload = f"Department: {departments_map.get(str(department_id), 'Unknown')}"
 
     context = {
-        'title': 'Na baba mi kaisiite',
-        'payload': payload,
+        "title": "Na baba mi kaisiite",
+        "payload": payload,
     }
 
-    return render(request, 'departments/details.html', context)
+    return render(request, "departments/details.html", context)
 
-    
+
+def details_error(request):
+    raise Http404
