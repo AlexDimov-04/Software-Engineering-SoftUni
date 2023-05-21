0d5b4ac8711940d73d8f57ecf8be970df80b9fb2
Alexander Dimov <alex.lemuel@gmail.com>
2023-05-21 23:45:42 +0300

    added application files logic

--- Python Web/Python Web Basics/departments_project/departments/urls.py
+++ Python Web/Python Web Basics/departments_project/departments/urls.py
@@ -1,3 +1,14 @@
+from django.urls import path
+from .views import index, details, details_template
+
 urlpatterns = [
-    path()
+    path("", index),
+    path("int/<int:department_id>/", details),
+    path("template/<int:department_id>/", details_template),
+    path("<department_id>/", details)
 ]
+
+
+# urls_arr = ["int/<int:department_id>/", "<department_id>/"]
+
+# urlpatterns = [path(url, details) for url in urls_arr]
--- Python Web/Python Web Basics/departments_project/departments/views.py
+++ Python Web/Python Web Basics/departments_project/departments/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    print(request)
+    return HttpResponse('index')
+
+
+def details(request, department_id):
+    departments_map = {
+        '1': "Developers",
+        '2': "QA"
+    }
+
+    payload = f"Department: {departments_map.get(str(department_id), 'Unknown')}"
+    # return HttpResponse(payload)
+    # return redirect('/departments/template/', department_id=department_id)
+    return 
+
+def details_template(request, department_id):
+    departments_map = {
+        '1': "Developers",
+        '2': "QA"
+    }
+
+    payload = f"Department: {departments_map.get(str(department_id), 'Unknown')}"
+
+    context = {
+        'title': 'Na baba mi kaisiite',
+        'payload': payload,
+    }
+
+    return render(request, 'departments/details.html', context)
+
+    
