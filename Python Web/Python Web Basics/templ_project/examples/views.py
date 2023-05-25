44139d11f2e5410b976270ae1e0a96e4e70a9528
Alexander Dimov <alex.lemuel@gmail.com>
2023-05-25 22:56:45 +0300

    added django file

--- Python Web/Python Web Basics/templ_project/examples/views.py
+++ Python Web/Python Web Basics/templ_project/examples/views.py
@@ -1,27 +1,46 @@
 import datetime
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-class Student():
+
+class Student:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def get_age(self):
         return self.age
-    
-some_student = Student('Alex', 18)
+
+
+some_student = Student("Alex", 18)
+
 
 def index(request):
     context = {
         "title": "Home",
         "random": random.randrange(1, 100),
-        'nested': {
-            'second': 'second value',
+        "nested": {
+            "second": "second value",
         },
-        'student': some_student,
-        'students': ['Aria', 'Emanuela', 'Erik'],
-        'now': datetime.date.today() 
+        "student": some_student,
+        "students": ["Aria", "Emanuela", "Erik", "Erik"],
+        "students_second": [],
+        "now": datetime.date.today(),
+        "numbers": [1, 2, 3, 4, 5],
+        'letters': ['a', 'e', 'i', 'o', 'g', 'r', 'b', 'w', 'b', 'p'],
+        'students_obj': [
+            Student('Lili', 23),
+            Student('Moni', 20),
+            Student('Yavor', 30)
+        ]
     }
 
-    return render(request, "examples/index.html", context=context)
+    return render(request, "examples/partials/index.html", context=context)
+
+
+def contact_view(request, name):
+    return redirect("index")
+
+
+def about_view(request):
+    return render(request, 'examples/partials/about.html')
