3f7f6b941cca166e1a15227ec5711bdfd9587f7b
Alexander Dimov <alex.lemuel@gmail.com>
2023-05-29 23:55:50 +0300

    added portfolio file

--- Python Web/Python Web Basics/Portfolio/Portfolio/settings.py
+++ Python Web/Python Web Basics/Portfolio/Portfolio/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
