965e437364b2e5fd818b80ddfe793b70b90df86b
Alexander Dimov <alex.lemuel@gmail.com>
2023-06-28 02:44:14 +0300

    added List and Detail view

--- Python Web/Python Web Framework/Exercises/cbv_demo/cbv_demo/web/urls.py
+++ Python Web/Python Web Framework/Exercises/cbv_demo/cbv_demo/web/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from cbv_demo.web.views import ArticlesListView, RedirectArticlesView
+from cbv_demo.web.views import ArticlesListView, RedirectArticlesView, ArticleDetailView
 from cbv_demo.web.views import list_articles
 
 
 urlpatterns = [
     path('', list_articles, name='list_articles'),
     path('cbv/', ArticlesListView.as_view(), name='list_articles_cbv'),
-    path('cbv/<int:pk>/', ArticlesListView.as_view(), name='list_articles_cbv'),
+    # path('cbv/<int:pk>/', ArticlesListView.as_view(), name='list_articles_cbv'),
     path('redirect-to-articles/', RedirectArticlesView.as_view(), name='redirect_to_articles'),
+    path('cbv/<int:pk>', ArticleDetailView.as_view(), name='details_article')
 ]
--- Python Web/Python Web Framework/Exercises/cbv_demo/cbv_demo/web/views.py
+++ Python Web/Python Web Framework/Exercises/cbv_demo/cbv_demo/web/views.py
@@ -34,7 +34,25 @@ class RedirectArticlesView(views.RedirectView):
     url = reverse_lazy('list_articles_cbv')
 
 class ArticlesListView(views.ListView):
+
+
     template_name = 'articles/list.html'
     model = Article
 
     paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        search = self.request.GET.get('search', '')
+        queryset = queryset.filter(title__icontains=search)
+        return queryset
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['search'] = self.request.GET.get('search', '')
+        return context
+    
+class ArticleDetailView(views.DetailView):
+    model = Article
+    template_name = 'articles/detail.html'
